53fec1e902dc21de6282fed33c257b5e011f219d
howeroc <howeroc@gmail.com>
2016-10-10 18:39:22 +0800

    修改获取代理方法

--- src/shared/TorProxy.py
+++ src/shared/TorProxy.py
@@ -5,15 +5,17 @@ class TorProxy:
 
     # signal TOR for a new connection
     @staticmethod
-    def change_ip(self):
+    def change_ip():
         with Controller.from_port(port = 9051) as controller:
             controller.authenticate(password="weiphone2011")
             controller.signal(Signal.NEWNYM)
 
     @staticmethod
-    def get_proxy(self):
-        self.change_ip()
-        proxies = {
-            'https': 'socks5://localhost:9050'
-        }
-        return proxies
+    def get_proxy():
+        with Controller.from_port(port = 9051) as controller:
+            controller.authenticate(password="weiphone2011")
+            controller.signal(Signal.NEWNYM)
+            proxies = {
+                'https': 'socks5://localhost:9050'
+            }
+            return proxies
