0ed42760caa6fa67fba8b743c49d02f178a7b487
howeroc <howeroc@gmail.com>
2016-10-11 12:01:50 +0800

    修改更新Tor代理ip地址的静态方法

--- src/shared/TorProxy.py
+++ src/shared/TorProxy.py
@@ -2,20 +2,16 @@ from stem import Signal
 from stem.control import Controller
 
 class TorProxy:
-
     # signal TOR for a new connection
     @staticmethod
     def change_ip():
         with Controller.from_port(port = 9051) as controller:
             controller.authenticate(password="weiphone2011")
             controller.signal(Signal.NEWNYM)
-
     @staticmethod
     def get_proxy():
         with Controller.from_port(port = 9051) as controller:
             controller.authenticate(password="weiphone2011")
             controller.signal(Signal.NEWNYM)
-            proxies = {
-                'https': 'socks5://localhost:9050'
-            }
-            return proxies
+        proxies = {'https': 'socks5://127.0.0.1:9050'}
+        return proxies
