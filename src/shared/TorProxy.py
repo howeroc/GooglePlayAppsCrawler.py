2ef47aedc351dcaadc4b3209cec478ad0de2bcf9
howeroc <howeroc@gmail.com>
2016-10-14 11:41:08 +0800

    [Update] Change change_id method into not static

--- src/shared/TorProxy.py
+++ src/shared/TorProxy.py
@@ -3,8 +3,7 @@ from stem.control import Controller
 
 class TorProxy:
     # signal TOR for a new connection
-    @staticmethod
-    def change_ip():
+    def change_ip(self):
         with Controller.from_port(port = 9051) as controller:
             controller.authenticate(password="weiphone2011")
             controller.signal(Signal.NEWNYM)
