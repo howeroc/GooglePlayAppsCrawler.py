b4f9af2f62fae144880a64488fedde93a0fdd894
howeroc <howeroc@gmail.com>
2016-10-14 14:37:21 +0800

    [Test] test

--- src/shared/TorProxy.py
+++ src/shared/TorProxy.py
@@ -13,5 +13,6 @@ class TorProxy:
         with Controller.from_port(port = 9051) as controller:
             controller.authenticate(password="weiphone2011")
             controller.signal(Signal.NEWNYM)
-        proxies = {'https': 'socks5://127.0.0.1:9050'}
+        proxies = {'http': 'socks5://127.0.0.1:9050',
+                   'https': 'socks5://127.0.0.1:9050'}
         return proxies
